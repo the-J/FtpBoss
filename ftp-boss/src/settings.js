b92c4765f581fab8c8c319a0a2f647af60d2928b
Juliusz Jakubowski <the-j@users.noreply.github.com>
2018-10-04 23:47:21 +0200

    yeah, maybe settings for all things that might be useful

---
+++ ftp-boss/src/settings.js
@@ -0,0 +1,13 @@
+/**
+ * Created by juliusz.jakubowski@gmail.com on 04.10.18.
+ */
+
+export const colors = {
+    'white': '#FFF',
+    'dark-blue': '#152642',
+    'blue': '#084575',
+    'light-cream': '#f3efd5',
+    'cream': '#e7d5a7',
+    'dark-cream': '#c0b2a4',
+    'accept': '#00ff00'
+};
