62579dab2ed163509973bdc7a829b184f268feeb
Juliusz Jakubowski <the-j@users.noreply.github.com>
2018-10-04 23:49:44 +0200

    keeping router in one place update

--- ftp-boss/src/index.js
+++ ftp-boss/src/index.js
@@ -2,20 +2,26 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'connected-react-router';
+import { Route, Switch } from 'react-router-dom';
+
 import 'semantic-ui-css/semantic.min.css';
+
+import * as serviceWorker from './serviceWorker';
 
 import store, { history } from './store/store';
 
 import App from './components/App';
-
-import * as serviceWorker from './serviceWorker';
+import Settings from './components/Settings';
 
 const target = document.querySelector('#root');
 
 render(
     <Provider store={store}>
         <ConnectedRouter history={history}>
-            <App />
+            <Switch>
+                <Route exact path="/" component={App} />
+                <Route exact path="/settings" component={Settings} />
+            </Switch>
         </ConnectedRouter>
     </Provider>,
     target);
