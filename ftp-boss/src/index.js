4146296be1b281f464f13a9c55f37e2d089a532d
Juliusz Jakubowski <the-j@users.noreply.github.com>
2018-10-04 22:37:32 +0200

    semantic-ui

--- ftp-boss/src/index.js
+++ ftp-boss/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'connected-react-router';
+import 'semantic-ui-css/semantic.min.css';
 
 import store, { history } from './store/store';
 
