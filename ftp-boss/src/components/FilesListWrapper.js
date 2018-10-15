c6af1fecc0928745e549dafac1ee0da20300579e
Juliusz Jakubowski <the-j@users.noreply.github.com>
2018-10-15 09:45:57 +0200

    updating FilesListWrapper to differ empty directory and lack of initial connection

--- ftp-boss/src/components/FilesListWrapper.js
+++ ftp-boss/src/components/FilesListWrapper.js
@@ -1,36 +1,30 @@
-import React, { Component } from 'react';
-
-import FilesList from './FilesList';
-
 /**
  * Created by juliusz.jakubowski@gmail.com on 14.10.18.
  */
 
+import React, { Component } from 'react';
+import { Container } from 'semantic-ui-react';
+
+import FilesList from './FilesList';
+
 
 class FilesListWrapper extends Component {
-    constructor() {
-        super();
-
-        this.state = {
-            list: [],
-            connectingFtp: false
-        };
-    }
-
     render() {
-        const { list, goToDirectory, downloadFile } = this.props;
+        const { list, goToDirectory, downloadFile, currentPath } = this.props;
 
         return (
-            <span>
-         {
-             list && !!list.length ?
-                 <FilesList
-                     list={list}
-                     goToDirectory={dir => goToDirectory(dir)}
-                     download={fileName => downloadFile(fileName)}
-                 /> : 'nothing to display'
-         }
-     </span>
+            <Container>
+                {
+                    currentPath.result || currentPath.result === '' ?
+                        list && !!list.length ?
+                            <FilesList
+                                list={list}
+                                goToDirectory={dir => goToDirectory(dir)}
+                                download={fileName => downloadFile(fileName)}
+                            /> : 'no files'
+                        : 'connect'
+                }
+            </Container>
         );
     }
 }
