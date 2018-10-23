c633c60e5be4f581e5d84ba45cb1494e7b41826c
Juliusz Jakubowski <the-j@users.noreply.github.com>
2018-10-23 22:47:52 +0200

    this wont be stateless component

--- ftp-boss/src/components/ModalAddFileOrDirectory.js
+++ ftp-boss/src/components/ModalAddFileOrDirectory.js
@@ -1,29 +1,43 @@
 /**
  * Created by juliusz.jakubowski@gmail.com on 19.10.18.
  */
-import React from 'react';
+import React, { Component } from 'react';
 import { Modal } from 'semantic-ui-react';
 
-export const ModalAddFileOrDirectory = props => (
-    <Modal
-        basic
-        size='small'
-        open={props.showUploadModal}
-        onClose={() => props.showHideModal()}
-    >
-        {/*<Header icon='archive' content='Archive Old Messages' />*/}
-        {/*<Modal.Content>*/}
-        {/*<p>*/}
-        {/*Your inbox is getting full, would you like us to enable automatic archiving of old messages?*/}
-        {/*</p>*/}
-        {/*</Modal.Content>*/}
-        {/*<Modal.Actions>*/}
-        {/*<Button basic color='red' inverted>*/}
-        {/*<Icon name='remove' /> No*/}
-        {/*</Button>*/}
-        {/*<Button color='green' inverted>*/}
-        {/*<Icon name='checkmark' /> Yes*/}
-        {/*</Button>*/}
-        {/*</Modal.Actions>*/}
-    </Modal>
-)
+
+class ModalAddFileOrDirectory extends Component {
+    constructor( props ) {
+        super(props);
+
+    }
+
+    render() {
+        const { showUploadModal, showHideModal } = this.props;
+
+        return (
+            <Modal
+                basic
+                size='small'
+                open={showUploadModal}
+                onClose={() => showHideModal()}
+            >
+                {/*<Header icon='archive' content='Archive Old Messages' />*/}
+                {/*<Modal.Content>*/}
+                {/*<p>*/}
+                {/*Your inbox is getting full, would you like us to enable automatic archiving of old messages?*/}
+                {/*</p>*/}
+                {/*</Modal.Content>*/}
+                {/*<Modal.Actions>*/}
+                {/*<Button basic color='red' inverted>*/}
+                {/*<Icon name='remove' /> No*/}
+                {/*</Button>*/}
+                {/*<Button color='green' inverted>*/}
+                {/*<Icon name='checkmark' /> Yes*/}
+                {/*</Button>*/}
+                {/*</Modal.Actions>*/}
+            </Modal>
+        );
+    }
+}
+
+export default ModalAddFileOrDirectory;
