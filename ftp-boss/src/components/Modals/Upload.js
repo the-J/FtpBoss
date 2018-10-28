f47596a4fe00301f5c493e990fa7943c8d339423
Juliusz Jakubowski <the-j@users.noreply.github.com>
2018-10-28 22:53:28 +0100

    basic onDrop field

--- ftp-boss/src/components/Modals/Upload.js
+++ ftp-boss/src/components/Modals/Upload.js
@@ -1,3 +1,44 @@
 /**
  * Created by juliusz.jakubowski@gmail.com on 27.10.18.
  */
+
+import React, { Fragment } from 'react';
+import { Button, Header, Modal } from 'semantic-ui-react';
+import Dropzone from 'react-dropzone';
+
+
+const CreateDirectory = props => (
+    <Fragment>
+        <Header icon='file' content='Upload files' />
+
+        <Modal.Content>
+            <h4>Drop files here</h4>
+            <Dropzone onDrop={e => {
+                if (!e.length) return console.log('no files droped');
+
+                props.handdleOnDrop(e)
+            }} />
+        </Modal.Content>
+
+        <Modal.Actions>
+            <Button
+                inverted
+                icon='remove'
+                content='Abort'
+                color='red'
+                onClick={() => props.showHideModal()}
+            />
+
+            <Button
+                inverted
+                icon='checkmark'
+                content='Create'
+                color='green'
+                disabled={!props.newDirectoryName}
+                onClick={() => props.uploadFiles()}
+            />
+        </Modal.Actions>
+    </Fragment>
+);
+
+export default CreateDirectory;
